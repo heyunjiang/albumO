22b31425c32681d3282ecd89816b77547a628cde
heyunjiang <598119677@qq.com>
2016-05-10 15:14:44 +0800

    add sql

--- design/albumo.sql
+++ design/albumo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2016 年 05 月 08 日 12:10
+-- 生成日期: 2016 年 05 月 09 日 15:17
 -- 服务器版本: 5.5.47
 -- PHP 版本: 5.3.29
 
@@ -48,6 +48,7 @@ CREATE TABLE IF NOT EXISTS `img` (
   `img_url` varchar(500) NOT NULL,
   `img_description` varchar(500) DEFAULT NULL,
   `img_click` bigint(20) DEFAULT NULL,
+  `img_main` char(2) NOT NULL DEFAULT '0',
   PRIMARY KEY (`img_id`),
   KEY `FK_img_album` (`ac_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -102,7 +103,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `power` char(2) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`email`, `password`, `nickname`, `sex`, `headurl`, `user_id`, `power`) VALUES
+('598119677@qq.com', '111111', '小江哥', '0', '', 1, '2');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
